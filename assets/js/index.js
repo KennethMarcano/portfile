f47a63977af4b09bb54ec8c3aee5724ad761e9a1
Kenneth <kennethmarcano96@outlook.com>
2024-07-11 00:11:41 -0300

    'addedMenuBarLeft'

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,16 +5,34 @@ window.addEventListener('scroll', () => {
     var distance1 = contactMe1.getBoundingClientRect().top;
     var distance2 = contactMe2.getBoundingClientRect().top;
     if (distance1 <= heightWindow * 0.8) {
-        contactMe1.classList.add('show')
+        contactMe1.classList.add('show1')
     }
     else {
-        contactMe1.classList.remove('show')
+        contactMe1.classList.remove('show1')
     }
     if (distance2 <= heightWindow*0.7 ) {
-        contactMe2.classList.add('show')
+        contactMe2.classList.add('show1')
     }
     else {
-        contactMe2.classList.remove('show')
+        contactMe2.classList.remove('show1')
     }
 
+})
+var openMenu = false;
+const closeMenu = document.getElementById('close-menu')
+closeMenu.addEventListener('click', () => {
+    const menu =  document.getElementById('menu');
+    const menuContent = document.getElementById('menu-content');
+    openMenu = !openMenu;
+    if(openMenu){
+        menu.classList.add('show2');
+        menuContent.classList.add('show2');
+        closeMenu.textContent = '✕'
+    }
+    else{
+        menu.classList.remove('show2');
+        menuContent.classList.remove('show2');
+        closeMenu.textContent = '☰'
+    }
+    console.log('clickado');
 })
