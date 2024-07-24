4778b4a718e3fadb9d03fbe94feeddbe44714f03
Kenneth <kennethmarcano96@outlook.com>
2024-07-24 15:37:36 -0300

    fixed responsive section skills

--- assets/js/index.js
+++ assets/js/index.js
@@ -84,7 +84,7 @@ closeMenu.addEventListener('click', () => {
     if (openMenu) {
         menu.classList.add('show2');
         menuContent.classList.add('show2');
-        closeMenu.textContent = '✕ ';
+        closeMenu.textContent = '✕';
     }
     else {
         menu.classList.remove('show2');
